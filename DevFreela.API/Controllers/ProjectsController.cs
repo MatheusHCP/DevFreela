830b204b79f9169a0daa4b376d723cba2e7a62e7
Matheus Pereira <65426315+MatheusHCP@users.noreply.github.com>
2024-02-07 23:41:41 -0300

    Initial Commit

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+	[Route("api/projects")]
+	public class ProjectsController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult get()
+		{
+
+
+			return Ok();
+		}
+
+
+	}
+}
+
