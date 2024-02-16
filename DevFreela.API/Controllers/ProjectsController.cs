e90c960e0c3ef1575e3c4f3881e55c0a4e066014
Matheus Pereira <65426315+MatheusHCP@users.noreply.github.com>
2024-02-15 21:17:00 -0300

    feat(Controllers): adicionado novos endpoints para Projetos e Usuários.

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
@@ -7,15 +9,97 @@ namespace DevFreela.API.Controllers
 	public class ProjectsController : ControllerBase
 	{
 
+		private readonly IProjectService _projectService;
+
+		public ProjectsController(IProjectService projectService)
+		{
+			_projectService = projectService;
+		}
+
+		// api/projects
+
 		[HttpGet]
-		public IActionResult get()
+		public IActionResult GetAll(string query)
 		{
 
+			var projects = _projectService.GetAll(query);
+			
+			return Ok(projects);
+		}
+
+		// api/projects/1
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+
+            var project = _projectService.GetById(Id);
+
+			if (project == null)
+				return NoContent();
+
+            return Ok(project);
+        }
+
+		[HttpPost]
+		public IActionResult Post([FromBody] NewProjectInputModel inputModel)
+		{
+			if(inputModel.Title.Length > 20)
+			{
+				return BadRequest();
+			}
+
+			var id = _projectService.Create(inputModel);
+
+			return CreatedAtAction(nameof(GetById), new {id}, inputModel);
+		}
+
+        [HttpPut]
+        public IActionResult Put([FromBody] UpdateProjectInputModel updateProject)
+		{
+
+            if (updateProject.Title.Length > 20)
+            {
+                return BadRequest();
+            }
+
+            _projectService.Update(updateProject);
+
+            return Ok();
+		}
+
+		
+		[HttpDelete]
+		public IActionResult Delete(int id)
+		{
+			_projectService.Delete(id);
 
 			return Ok();
 		}
 
+		[HttpPost("{id}/comments")]
+		public IActionResult PostComment (int id, [FromBody] CreateCommentInputModel inputModel)
+		{
+			_projectService.CreateComment(inputModel);
 
-	}
+			return Ok();
+		}
+
+		[HttpPut("{id}/start")]
+		public IActionResult Start(int Id)
+		{
+			_projectService.Start(Id);
+
+			return Ok();
+		}
+
+		[HttpPut("{id}/finish")]
+		public IActionResult Finish(int id)
+		{
+			_projectService.Finish(id);
+
+			return Ok();
+		}
+    }
 }
 
---
+++ DevFreela.API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DevFreela.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+	[Route("api/users")]
+	public class UserController : ControllerBase
+	{
+
+		// api/users/1
+		[HttpGet("{id}")]
+		public IActionResult getById(int id)
+		{
+			return Ok();
+		}
+		// api/users
+		[HttpPost]
+        public IActionResult Post([FromBody] NewUserInputModel inputModel)
+		{
+			return Ok();
+		}
+
+		// api/users/1/login
+		[HttpPost("{id}/login")]
+		public IActionResult Login(int id, [FromBody] LoginInputModel login)
+		{
+			return NoContent();
+		}
+
+
+
+    }
+}
+
